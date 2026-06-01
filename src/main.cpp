690fcabc34b55a07b9c17ee1ba29e5a6649a466c
anton3798 <chervoniy.vladimir.152@gmail.com>
2026-06-01 23:11:09 +0300

    Update main1.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,7 @@
 // Copyright 2021 NNTU-CS
 #include "bst.h"
 #include <iostream>
+#include <string>
 
 void makeTree(BST<std::string>& tree, const char* filename);
 void printFreq(BST<std::string>& tree);
