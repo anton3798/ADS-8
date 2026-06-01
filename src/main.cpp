c25546807f1dbaac195d3f8e9fc0e08ba707c1ab
anton3798 <chervoniy.vladimir.152@gmail.com>
2026-06-01 22:31:12 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,15 @@
 // Copyright 2021 NNTU-CS
 #include "bst.h"
+#include <iostream>
+
+void makeTree(BST<std::string>& tree, const char* filename);
+void printFreq(BST<std::string>& tree);
 
 int main() {
+    BST<std::string> tree;
+    makeTree(tree, "src/war_peace.txt");
+    std::cout << "Tree depth: " << tree.depth() << std::endl;
+    std::cout << "Word 'war' is " << (tree.search("war") ? "" : "not ") << "found.\n\n";
+    printFreq(tree);
     return 0;
 }
