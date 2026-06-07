11afded7cec807dde2a5a6077e180cdaade2bdd8
anton3798 <chervoniy.vladimir.152@gmail.com>
2026-06-07 22:56:49 +0300

    Update alg3.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -12,14 +12,13 @@ void makeTree(BST<std::string>& tree, const char* filename) {
         std::cerr << "Ошибка открытия: " << filename << std::endl;
         return;
     }
+        std::locale loc("C");
     std::string word;
     char ch;
     while (file.get(ch)) {
-        if ((ch >= 'A' && ch <= 'Z') || (ch >= 'a' && ch <= 'z')) {
-            if (ch >= 'A' && ch <= 'Z') {
-                ch = ch + ('a' - 'A');
-            }
-            word += ch;
+        unsigned char uch = static_cast<unsigned char>(ch);
+        if (std::isalpha(uch, loc)) {
+            word += std::tolower(uch, loc);
         } else {
             if (!word.empty()) {
                 tree.insert(word);
