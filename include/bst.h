ddd702f4a9c9f4260cf2e3a9fe62f8634b3ead32
anton3798 <chervoniy.vladimir.152@gmail.com>
2026-06-07 22:43:52 +0300

    Update bst1.h

--- include/bst.h
+++ include/bst.h
@@ -13,7 +13,7 @@ class BST {
         int count;
         Node* left;
         Node* right;
-        Node(const T& k) : key(k), count(1), left(nullptr), right(nullptr) {}
+        explicit Node(const T& k) : key(k), count(1), left(nullptr), right(nullptr) {}
     };
 
     Node* root;
